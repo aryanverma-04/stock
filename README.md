d743f1f7a95b641d1e0d4abdcdb3048389d3cf7c
Aryan verma <93571501+aryanverma-04@users.noreply.github.com>
2023-03-29 16:36:09 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+[![pages-build-deployment](https://github.com/StasonJatham/bearbot3/actions/workflows/pages/pages-build-deployment/badge.svg)](https://github.com/StasonJatham/bearbot3/actions/workflows/pages/pages-build-deployment) [![ManualStonks](https://github.com/StasonJatham/bearbot3/actions/workflows/manup.yml/badge.svg)](https://github.com/StasonJatham/bearbot3/actions/workflows/manup.yml) [![Stonks](https://github.com/StasonJatham/bearbot3/actions/workflows/update.yml/badge.svg)](https://github.com/StasonJatham/bearbot3/actions/workflows/update.yml)
+
+# Bearbot 3.0 - Finding Dips and making Tendies
+
+<img src="static/bearbot_big_hero.png" alt="Bearbot Logo" width="200"/>
+
+## Installation
+Pretty straight forward to get started.
+```python3
+mkdir data
+pip install -r requirements.txt
+python3 dl.py
+```
+This will then pull prices for Symbols in `symbols.lst`, calculate the biggest consecutive loss over the last 365 days and the save it to `data/YYYY-MM-DD_SYMBOL.json`. The JSON Files are more like of a "state" keeping mechanism (because this was originally running in AWS Batch with Spot EC2), where the can terminate your instance. The way I built it, it should be fault tolerant for these kind of interruptions, on restart it would just keep going.
+### Adding & Removing Symbols
+If you want to add or remove symbols you just edit the `symbols.lst`.
+
+## Infrastructure 
+Before I refactored Bearbot, it was running on a t4g.small in ca-central-1 (0,0184 USD/hour,2vCPU, 2 GiB RAM) and built with Django an SQLite. That`s about $14 a month. I chose their Gravitron instances, because I love their performance and evironmental impact, that is also why I host my compute in Canada 🇨🇦,
+[the ca-centra-1 zone is apparently being powered 99.5% by Hydropower](http://news.hydroquebec.com/en/press-releases/960/green-electricity-attracts-amazon-web-services-to-montreal/). I am just assuming by that fact alone that it must be one of their most sustainable zones (sadly aws does not supply that sort of data per zone).
+
+
+You see the simplicity of this App. Would you pay $14 a month for it? Nope. I was looking for a cheaper way and started refactoring for AWS Batch + S3. I figured I cloud use some Spot EC2 for compute, run my script and save to S3 where I also planned on hosting. It worked so far as well, hence `dl.py` was built for interruptabillity.
+
+I then thought, wait a minute GitHub is carbon neutral as well, and I could do this with GitHub Pages and GitHub Actions for absolutely free. Here we are.
---
+++ dl.py
@@ -0,0 +1,102 @@
+# -> standard
+import json
+import glob
+from os import listdir, remove
+from datetime import datetime
+from os.path import isfile, join
+from typing import Any
+# -> third party
+import pandas as pd
+import requests_cache
+import yfinance as yf
+
+
+def get_all_symbols() -> list[str]:
+    with open('symbols.lst', 'r') as symbols:
+        return symbols.read().split()
+
+
+def calculate_loss(prices: pd.DataFrame) -> tuple[int, float]:
+    last_price: float = prices["Close"][-1]
+    last_date = prices.index[-1]
+    highest_loss: tuple[float, Any] = (9999999999999999, 0)
+
+    for date_c, row in prices[::-1].iterrows():
+        if (last_price / row["Close"] - 1) < highest_loss[0]:
+            highest_loss = ((last_price / row["Close"] - 1), date_c)
+
+    delta: int = highest_loss[1] - last_date
+
+    return (abs(delta.days), highest_loss[0] * 100)
+
+
+def main():
+
+    # -> Symbols to update
+    for f in stock_jsons:
+        current_symbol: str = f.split("_")[1].split(".json")[0]
+        last_update = datetime.strptime(f.split("_")[0], '%Y-%m-%d').date()
+        if today < last_update:
+            old_symbols.append(current_symbol)
+
+    s = set(old_symbols)
+    symbols = [x for x in get_all_symbols() if x not in s]
+
+    for sym in symbols:
+        latest = datetime.today().strftime('%Y-%m-%d')
+
+        for f in glob.glob(f"data/*_{sym}.json"):
+            if latest not in f:
+                print(f"-- Removing old: {f}")
+                remove(f)
+
+        print(f"++ Adding {sym}")
+        format_datatable: dict[str, list[list[str | int | float]]] = {
+            "data": []
+        }
+
+        data = yf.Tickers(sym)
+        full_name: str = data.tickers[sym].info["longName"]
+        hist = pd.DataFrame(data.tickers[sym].history(period="1y"))
+        hist.index = pd.to_datetime(hist.index)
+        hist = hist[-200:]
+
+        days, loss = calculate_loss(hist)
+
+        stonk: dict[str, str | float] = {
+            "name": full_name,
+            "symbol": sym,
+            "latest": latest,
+            "days": days,
+            "loss": loss,
+        }
+
+        with open(f"data/{latest}_{sym}.json", 'w') as name:
+            name.write(json.dumps(stonk))
+
+        jsonfiles = [f for f in listdir("data") if isfile(
+            join("data", f)) and ".json" in f]
+        for file in jsonfiles:
+            with open(f"data/{file}", 'r') as data:
+                x = json.loads(data.read())
+                format_datatable["data"].append(
+                    [x["name"], x["symbol"], x["days"], x["loss"], x["latest"]])
+
+        with open("all.json", 'w') as all:
+            all.write(json.dumps(format_datatable))
+
+        format_datatable["data"] = []
+
+
+if __name__ == "__main__":
+    session = requests_cache.CachedSession("yfinance.cache")
+    session.headers[
+        "User-agent"
+    ] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:98.0) Gecko/20100101 Firefox/98.0"
+    old_symbols: list[str] = []
+
+    today = datetime.now().date()
+    stock_jsons = [f for f in listdir("data") if isfile(
+        join("data", f)) and ".json" in f]
+
+    main()
